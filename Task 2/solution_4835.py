4cbcbe7d7f61453e78d03745e45acbb28111e6ae
ns <nailyasalikhova66@gmail.com>
2022-09-06 19:17:28 +0300

    fix

--- Task 2/solution_4835.py
+++ Task 2/solution_4835.py
@@ -3,5 +3,5 @@ for x in range(2):
     for y in range(2):
         for z in range(2):
             for w in range(2):
-                if not(((w <= y) and (not(bool(x)) <= z)) <= ((z == w) or (y and not(bool(x))))):
+                if not(((w <= y) and ((not(bool(x))) <= z)) <= ((z == w) or (y and (not(bool(x)))))):
                     print(x, y, z, w)
