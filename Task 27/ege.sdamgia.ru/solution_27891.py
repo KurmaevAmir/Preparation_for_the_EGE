8374ef9f71903ced4927d40664c7d9470ba3057d
KurmaevAmir <amirku05@yandex.ru>
2022-11-01 15:50:57 +0300

    Task 27

--- Task 27/ege.sdamgia.ru/solution_27891.py
+++ Task 27/ege.sdamgia.ru/solution_27891.py
@@ -3,6 +3,8 @@ def analyze_file(file):
         n = int(f.readline())
         f_sum = 0
         max_n14 = 0
+        max_n7 = 0
+        max_n2 = 0
         max_n = 0
         for i in range(n):
             number = int(f.readline())
@@ -11,8 +13,12 @@ def analyze_file(file):
                 # но не делится на 7. Еще вариант, что наибольшим окажется произведение двух наибольших чисел,
                 # делящихся на 14.
                 max_n14 = max(max_n14, number)
+            elif number % 7 == 0:
+                max_n7 = max(max_n7, number)
+            elif number % 2 == 0:
+                max_n2 = max(max_n2, number)
             max_n = max(max_n, number)
-        print(max_n * max_n14)
+        print(max((max_n * max_n14), (max_n2 * max_n7)))
 
 
 analyze_file("Input_files_27891/27-A_2.txt")
