f31752ec73f8461a6655f044d2f09aa997d2ca01
Nailia Salikhova <nsalikhova@avtodoria.ru>
2023-04-29 04:06:06 +0300

    Task 23

--- Task 23/solution_3719(Решение).py
+++ Task 23/solution_3719(Решение).py
@@ -1,8 +1,9 @@
 massive = [0] * 17
 massive[1] = 1
-massive[2] = 1
-for i in range(3, 17):
-    a = massive[i - 2]
+for i in range(2, 17):
+    a = 0
+    if i - 2 >= 1:
+        a += massive[i - 2]
     if i - 3 >= 1:
         a += massive[i - 3]
     if i % 4 == 0 and i // 4 >= 1:
---
+++ Task 23/solution_5931(Решение).py
@@ -0,0 +1,12 @@
+massive = [0] * 53
+massive[50] = 1
+for i in range(49, 0, -1):
+    a = 0
+    if i + 3 <= 50:
+        a += massive[i + 3]
+    for j in range(0, 7):
+        if i * 7 + j <= 50:
+            a += massive[i * 7 + j]
+    massive[i] = a
+print(massive[1])
+
