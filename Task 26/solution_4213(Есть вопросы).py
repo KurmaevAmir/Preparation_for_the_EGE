ed9fb1116056d2a647e14567a8e3f4eab05a6147
Nailia Salikhova <nsalikhova@avtodoria.ru>
2023-06-08 02:14:34 +0300

    Task 26 Номер 4213

--- Task 26/solution_4213(Есть вопросы).py
+++ Task 26/solution_4213(Есть вопросы).py
@@ -3,33 +3,40 @@ with open("Files/4213", "r") as f:
     mass_input = []
     for i in range(n):
         mass_input.append(int(f.readline().strip()))
-    mass_input.sort()
+    mass_input_set = set(mass_input)
     mass = []
-    for number in mass_input:
+    for number in mass_input_set:
         if mass_input.count(number) >= 2 and number not in mass:
             mass.append((number, mass_input.count(number)))
+    mass.sort()
+    print(mass)
     total_list = []
+    total_count_list = []
     for i in range(len(mass)):
         if sum(total_list) + mass[i][0] > s:
             break
-        elif sum(total_list) + (mass[i][0] * mass[i][1]) < s:
+        elif sum(total_list) + (mass[i][0] * mass[i][1]) <= s:
             for j in range(mass[i][1]):
                 total_list.append(mass[i][0])
+            total_count_list.append(mass[i][1])
         else:
             count = mass[i][1]
             while sum(total_list) + (mass[i][0] * count) > s:
                 count -= 1
-            if count >= 3:
+            if count >= 2:
                 for j in range(count):
                     total_list.append(mass[i][0])
-    # difference = s - sum(total_list)
-    # for i in range(len(total_list), len(mass)):
-    #     for j in range(2, mass[i][0]):
-    #         if mass[i][j] - mass[len(total_list) - 1][0] <= difference:
-    #             max_number = (mass[i][0], mass[i][1])
-    # for i in range(max_number[1]):
-    #     total_list.append(max_number[0])
-    # count = -float('inf')
-    # for number in total_list:
-    #     count = max(total_list.count(number), count)
-    # print(sum(total_list), count)
+            total_count_list.append(count)
+    sum_total = sum(total_list)
+    difference = s - sum_total
+    k = len(total_count_list)
+    for j in range(-count, 0, 2):
+        for i in range(k, len(mass)):
+            if 2 * mass[i][0] - 2 * total_list[j] <= difference:
+                sum_total -= 2 * total_list[j]
+                sum_total += 2 * mass[i][0]
+                difference = s - sum_total
+                k += 1
+                total_list[j] = mass[i][0]
+                total_list[j + 1] = mass[i][0]
+    print(max(total_list), max(total_count_list))
