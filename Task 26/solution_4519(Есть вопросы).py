676c3abcb9e9ebf67fdbe84025483d44746dc960
Nailia Salikhova <nsalikhova@avtodoria.ru>
2023-06-08 01:31:13 +0300

    Task 26 Номер 4519

--- Task 26/solution_4519(Есть вопросы).py
+++ Task 26/solution_4519(Есть вопросы).py
@@ -12,8 +12,17 @@ with open("Files/4519", "r") as f:
             break
         sum_coasts += mass[i][0]
         abc_list.append(mass[i][1])
-    difference = m - sum_coasts
-    for i in range(len(abc_list), n):
-        if mass[i][0] - mass[len(abc_list) - 1][0] <= difference:
-            max_number = mass[i][0]
-    print(abc_list.count("B"))
+    l = len(abc_list)
+    for j in range(len(abc_list) - 1, -1, -1):
+        difference = m - sum_coasts
+        if mass[j][1] != 'B' and difference > 0:
+            k = mass[j][0]
+            for i in range(l, n):
+                if mass[i][0] - k <= difference and mass[i][1] == 'B':
+                    sum_coasts -= k
+                    sum_coasts += mass[i][0]
+                    l = i + 1
+                    difference = m - sum_coasts
+                    abc_list[j] = mass[i][1]
+                    break
+print(abc_list.count("B"), m - sum_coasts)
