8f2c98e7d13efd708f2289338d4d2032935995ab
Nailia Salikhova <nsalikhova@avtodoria.ru>
2023-06-12 16:59:36 +0300

    Task 26 Номер 4741

--- Task 26/solution_4741(Есть вопросы).py
+++ Task 26/solution_4741(Есть вопросы).py
@@ -6,23 +6,23 @@ with open("Files/4741", "r") as f:
     max_time = 0
     start_time = k
     end_time = k + 24 * 3600000
-    for i in range(n):
-        numbers = [int(i) for i in f.readline().strip().split()]
-        if numbers[1] == 0:
-            numbers[1] = end_time
-        if numbers[0] >= end_time or numbers[1] <= start_time:
-            continue
-        mass.append([(max(start_time, numbers[0]), 1), (min(numbers[1], end_time, - 1))])
-    mass.sort()
-    a = 0
-    b = 0
-    c = 0
-    t0 = 0
-    for t, d in mass:
-        b += d
-        if b > a:
-            a, c = b, 0
-        if b - d == a:
-            c += t - t0
-        t0 = t
-    print(a, c)
+    time_process = [0 for i in range(24 * 3600000 + 1)]
+    print(len(time_process))
+    for i in f:
+        start_process, end_process = [int(j) for j in i.strip().split()]
+        if (start_process < start_time) and ((end_process > start_time) or (end_process == 0)):
+            count = count + 1
+        if (start_process >= start_time) and (start_process <= end_time):
+            time_process[start_process - start_time] = time_process[start_process - start_time] + 1
+        if (end_process >= start_time) and (int(end_process) <= end_time):
+            time_process[end_process - start_time] = time_process[end_process - start_time] - 1
+    sum_time = 0
+    max_process = 0
+    for i in range(1, 24 * 3600000):
+        count = count + time_process[i]
+        if count > max_process:
+            max_process = count
+            sum_time = 0
+        if count == max_process:
+            sum_time = sum_time + 1
+    print(max_process, sum_time)
